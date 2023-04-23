f7433b0fd89726da098979d7b12c63d240ba6415
JL202304 <131579953+JL202304@users.noreply.github.com>
2023-04-23 15:49:31 +0800

    Update Key.java

--- java/app/src/main/java/com/example/iapdemo/common/Key.java
+++ java/app/src/main/java/com/example/iapdemo/common/Key.java
@@ -17,7 +17,7 @@
 package com.example.iapdemo.common;
 
 public class Key {
-    private static final String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkW4zxYG8CgJfwUHUGsxNhCD8yxaRVfTMuI4EUy7Y3D46EWTcJtTvEycj+JjTbf4oKx1qspAAT7G+rIkiYubnPezI2KCEvp4PQmawxG6UB7bnWEiF9TdEXw+CrKONKgMKllhvB3AL39S8B/vPOlYw3cF/aKpcWY6CqlDn1Um6E3O3dfoIJ0iMpOMyzJE7chipZ7zxvL61kpEzlSP1hU2LyfRZtEBocmB6wj3FApdc33ao0wBsWtL7Ulvo5V6gCZrMo+AgYFgv+NKowD6MPWSRFpNqz+as+yfXah/dNTcWieGeFxA7kb8VqOj+HJEH4gjWBSN7ATXEYEkHBOpTLFK+1QIDAQAB";
+    private static final String PUBLIC_KEY = "your public key";
 
     /**
      * get the publicKey of the application
@@ -25,6 +25,6 @@ public class Key {
      * @return
      */
     public static String getPublicKey(){
-        return publicKey;
+        return PUBLIC_KEY;
     }
 }
